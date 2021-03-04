76670786dd96e89a0bc0c4eedc9e9db01a1ac1e9
FrostSource <jjhfrost@gmail.com>
2021-03-05 11:33:40 +1300

    changes

--- scripts/vscripts/spawning.lua
+++ scripts/vscripts/spawning.lua
@@ -1,23 +1,38 @@
 
 local ItemPool = {
+    -- 1st floor (base)
     {
         { class = 'item_hlvr_clip_energygun', weight = 1 },
         { class = 'item_hlvr_clip_energygun_multiple', weight = 0.1 },
         { class = 'item_healthvial', weight = 0.5 },
+        { class = 'item_hlvr_grenade_frag', weight = 0.02 },
     },
+    -- 2nd floor (damaged)
     {
         { class = 'item_hlvr_clip_energygun', weight = 1 },
         { class = 'item_hlvr_clip_energygun_multiple', weight = 0.1 },
         { class = 'item_healthvial', weight = 0.5 },
+        { class = 'item_hlvr_grenade_frag', weight = 0.02 },
         { class = 'item_hlvr_clip_shotgun_single', weight = 0.6 },
         { class = 'item_hlvr_clip_shotgun_multiple', weight = 0.1 },
     },
+    -- 3rd floor (construction)
     {
         { class = 'item_hlvr_clip_energygun', weight = 1 },
         { class = 'item_hlvr_clip_energygun_multiple', weight = 0.1 },
         { class = 'item_healthvial', weight = 0.5 },
+        { class = 'item_hlvr_grenade_frag', weight = 0.02 },
         { class = 'item_hlvr_clip_shotgun_single', weight = 0.6 },
         { class = 'item_hlvr_clip_shotgun_multiple', weight = 0.1 },
+        { class = 'item_hlvr_clip_rapidfire', weight = 0.5 },
+    },
+    -- 4th floor (white arena)
+    {
+        { class = 'item_hlvr_clip_energygun', weight = 1 },
+        { class = 'item_hlvr_clip_energygun_multiple', weight = 0.15 },
+        { class = 'item_healthvial', weight = 0.6 },
+        { class = 'item_hlvr_clip_shotgun_single', weight = 0.7 },
+        { class = 'item_hlvr_clip_shotgun_multiple', weight = 0.15 },
         { class = 'item_hlvr_clip_rapidfire', weight = 0.5 },
     },
 }
@@ -91,18 +106,21 @@ function SpawnKeycard(target_name, keycard_name, keycard_color, keycard_skin)
     keycard_skin = keycard_skin or 'default'
 
     local targets = Entities:FindAllByName(target_name)
+    print('Found',#targets,'with name',target_name)
     if #targets == 0 then
         print('Attempt to spawn keycard - No target entity found!')
         return
     end
 
     -- Grab a random target
-    local target = targets[RandomInt(1,#targets)]
+    local rnd = RandomInt(1,#targets)
+    print('chose',rnd)
+    local target = targets[rnd]
 
     -- Spawn keycard
     SpawnEntityFromTableSynchronous('prop_physics', {
         origin = target:GetOrigin(),
-        angles = RandomInt(0,359)..' '..RandomInt(0,359)..' '..RandomInt(0,359),
+        angles = RandomInt(0,120)..' '..RandomInt(0,120)..' '..RandomInt(0,120),
 
         damagefilter = '@filter_damage_keycard',
         targetname = keycard_name,
@@ -111,5 +129,6 @@ function SpawnKeycard(target_name, keycard_name, keycard_color, keycard_skin)
         rendercolor = keycard_color..' 255',
         CanDepositInItemHolder = '1'
     })
+    debugoverlay:Sphere(target:GetOrigin(), 16, 0, 255, 0, 255, true, 60)
 end
 
