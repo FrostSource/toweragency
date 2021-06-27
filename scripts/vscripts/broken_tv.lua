7f3df118a5759a01605730f7e5a427224fe38f9f
FrostSource <jjhfrost@gmail.com>
2021-06-28 08:05:12 +1200

    Added tv prefab for ending.
    Readded tv material overrides.

--- scripts/vscripts/broken_tv.lua
+++ scripts/vscripts/broken_tv.lua
@@ -1,9 +1,21 @@
 
 print("Broken tv activated")
-thisEntity:RedirectOutput("OnPlayerPickup", "OnPlayerPickup", thisEntity)
+--thisEntity:RedirectOutput("OnPlayerPickup", "OnPlayerPickup", thisEntity)
 
-function OnPlayerPickup()
-    print("Broken tv has been picked up")
-    DoEntFire("ending_tv_picked_up", "Trigger", "", 0, nil, nil)
-    thisEntity:DisconnectRedirectedOutput("OnPlayerPickup", "OnPlayerPickup", thisEntity)
-end
+--function OnPlayerPickup()
+--    print("Broken tv has been picked up")
+--    DoEntFire("ending_tv_picked_up", "Trigger", "", 0, nil, nil)
+--    thisEntity:DisconnectRedirectedOutput("OnPlayerPickup", "OnPlayerPickup", thisEntity)
+--end
+
+--function Particles(data)
+    --local tv = data.activator
+    local target = SpawnEntityFromTableSynchronous("info_target", {})
+    target:SetForwardVector(thisEntity:GetRightVector() * -1)
+    target:SetParent(thisEntity, "")
+    target:SetLocalOrigin(Vector(0,-6,6))
+    local p = ParticleManager:CreateParticle("particles/creatures/blind_zombie/blind_zombie_vomit_01.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
+    ParticleManager:SetParticleControlEnt(p, 1, target, PATTACH_ABSORIGIN, "", Vector(0,0,0), false)
+    --ParticleManager:SetParticleControlForward(p, 1, tv:GetRightVector() * -1)
+    thisEntity:SetHealth(9999)
+--end
